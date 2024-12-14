be40e7dc31cb1da0f40891ea2f0bb51c7004ad24
memadhavkh <khandelwalmadhav5@gmail.com>
2024-12-14 22:54:06 +0530

    Finally Implemented Custom Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,51 +1,45 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-// import { NextResponse } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing']);
 
+export default clerkMiddleware( async (auth, req) => {
+    if(!isPublicRoute(req)){
+        await auth.protect();
+    }
+    const url = req.nextUrl;
+    const searchParams = url.searchParams.toString();
+    const hostname = req.headers;
 
+    const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}`: ''}`;
 
-// export default clerkMiddleware((req) => {
-//   const url = req.nextUrl;
-//   const searchParams = url.searchParams.toString()
-//   const hostname = req.headers.get('host')
-//   const pathWithSearchParams = `${url.pathname}${
-//     searchParams.length > 0 ? `?${searchParams}` : ''
-//   }`
+    // if subdomain exists
+    const customSubDomain = hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0];
 
-//   // Handle subdomains
-//   const customSubDomain = hostname
-//     ?.split(process.env.NEXT_PUBLIC_DOMAIN || '')
-//     .filter(Boolean)[0]
+    if(customSubDomain){
+        return NextResponse.rewrite(
+            new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+        )
+    }
 
-//   if (customSubDomain) {
-//     return NextResponse.rewrite(
-//       new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
-//     )
-//   }
+    if(url.pathname === '/sign-in' || url.pathname === '/sign-up'){
+        return NextResponse.redirect(new URL('/agency/sign-in', req.url));
+    }
 
-//   // Redirect /sign-in and /sign-up to /agency/sign-in
-//   if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
-//     return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
-//   }
+    if(url.pathname === '/' || (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)){
+        return NextResponse.rewrite(new URL('/site', req.url))
+    };
 
-//   // Rewrite root (/) and /site to /site
-//   if (
-//     url.pathname === '/' ||
-//     (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
-//   ) {
-//     return NextResponse.rewrite(new URL('/site', req.url))
-//   }
+    if(url.pathname.startsWith('/agency') || url.pathname.startsWith('/subaccount')){
+        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+    }
+});
 
-//   // Rewrite specific paths
-//   if (
-//     url.pathname.startsWith('/agency') ||
-//     url.pathname.startsWith('/subaccount')
-//   ) {
-//     return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
-//   }
-
-//   // Allow the request to proceed for other routes
-//   return NextResponse.next()
-// })
-
+export const config = {
+    matcher: [
+      // Skip Next.js internals and all static files, unless found in search params
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      // Always run for API routes
+      '/(api|trpc)(.*)',
+    ],
+  }
